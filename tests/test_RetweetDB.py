fcf8690f392facfe2ef52e5afff01581b3b338d9
Jkb <jkb@jkbworld.com>
2016-11-11 22:48:19 -0800

    Comments and a little cleanup.

--- tests/test_RetweetDB.py
+++ tests/test_RetweetDB.py
@@ -8,7 +8,8 @@ from classes import RetweetDB
 class test_RetweetDB(unittest.TestCase):
     def setUp(self):
 
-        # Specify the file with your DB connection.
+        # Specify the YAML file with your DB connection.
+        # See sample.yaml.txt file in the sample folder.
         filename = "../jkbbotfactory.yaml"
 
         try:
@@ -18,6 +19,8 @@ class test_RetweetDB(unittest.TestCase):
             print "Error Opening Config File: " + filename
             exit(1)
 
+        # Pull the settings below from the YAML and build the
+        # database connection to the RetweetDB
         self.rtdb = RetweetDB.RetweetDB(
             yml['retweetdb']['odbc_driver'],
             yml['retweetdb']['hostname'],
@@ -29,6 +32,7 @@ class test_RetweetDB(unittest.TestCase):
         )
 
     def tearDown(self):
+        # Close the DB when we're done with it
         self.rtdb.close()
 
     def test_pass(self):
@@ -38,10 +42,10 @@ class test_RetweetDB(unittest.TestCase):
     def test_isTweetLogged(self):
         """ Test the is_tweet_logged() Function """
 
-        # This long number doesn't exist
+        # This long number doesn't exist, so this should be a False
         self.assertEquals(self.rtdb.is_tweet_logged("4444444777777444444444444444444447"), False)
 
-        # Insert a Tweet into the table, test for it, then delete it when we're done.
+        # Insert a Tweet into the table, test for it to get a True, then delete it when we're done.
         self.rtdb.log_tweet('4444444777777444444444', 'UnitTest', 'PyUnit')
         self.assertEquals(self.rtdb.is_tweet_logged('4444444777777444444444'), True)
         self.rtdb.delete_tweet('4444444777777444444444')
